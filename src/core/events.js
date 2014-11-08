a6ee1d622919208d3000c2691d1b1faf941a7304
Dan Englender <denglend+spade@gmail.com>
2014-11-08 15:32:00 -0500

    Moved rollup from core to its own panel

--- src/core/events.js
+++ src/core/events.js
@@ -1,5 +1,5 @@
 /* globals d3, window,Redraw,SetSelectValuesFromHash,Globals,SpadeSettings,CreateDomElement,CreateAdvancedOption,
-   ReloadDataSet,ResetMainDivHeight,ReadSelectValues,VisualizationChangeHandlerNoRedraw,GetPanel,document */
+   ReloadDataSet,ResetMainDivHeight,ReadSelectValues,VisualizationChangeHandlerNoRedraw */
 function SetEventHandlers() {
 	
 	d3.select(window).on('resize', Redraw); 
@@ -20,15 +20,4 @@ function SetEventHandlers() {
 			Globals.IgnoreHashChange = false;
 		}
 	};
-}
-
-function ToggleHeaderRollup() {
-	d3.select("#HeaderDiv").classed("HeaderOpen",function() {return !d3.select(this).classed("HeaderOpen");});
-	d3.select("#HeaderDiv").classed("HeaderClosed",function() {return !d3.select(this).classed("HeaderClosed");});
-	document.getElementById("HeaderDiv").scrollTop = 0;
-	if (SpadeSettings.Visualizations[GetPanel("VisualizationPanel").Options.CurVisualization].Settings.RedrawOnVerticalResize !== false) {
-		Redraw();
-	}
-
-
 }
